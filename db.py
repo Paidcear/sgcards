e14d004b4ae2891ee41f5a30c95b84aafce52654
Julio César Delgado Mora <60369136+Paidcear@users.noreply.github.com>
2025-12-08 01:07:38 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,40 +1,11 @@
-# db.py
-# -------------------------------------
-# Conexión a MySQL (Clever Cloud)
-# -------------------------------------
-
+import streamlit as st
 from sqlalchemy import create_engine
 
-# --------------------------------------------------------
-# 🔧 PON TUS DATOS REALES AQUI
-# --------------------------------------------------------
+HOST = st.secrets["DB_HOST"]
+USER = st.secrets["DB_USER"]
+PASSWD = st.secrets["DB_PASS"]
+DB = st.secrets["DB_NAME"]
+PORT = st.secrets["DB_PORT"]
 
-USER = "uenzqgunbvjgknad"
-PASSWORD = "8WzopsyqtDhBI7ZPnVQU"
-HOST = "bbg3b0loqlocqhpufv6m-mysql.services.clever-cloud.com"             # ej: xxxxxx.clever-cloud.com
-DATABASE = "bbg3b0loqlocqhpufv6m"
-PORT = 3306                  # MySQL usa 3306
-
-# --------------------------------------------------------
-# 🔗 Crear motor SQLAlchemy
-# --------------------------------------------------------
-engine = create_engine(
-    f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}",
-    pool_pre_ping=True
-)
-
-
-# notifications.py
-import requests
-
-TELEGRAM_TOKEN = "8476867150:AAEP1mvaHz04oHYoEV0S8EgCpiEjU4hOQc0"
-CHAT_ID = "2090371962"
-
-def enviar_notificacion(mensaje):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": mensaje
-    }
-    requests.post(url, json=payload)
-
+# Crear motor de SQLAlchemy
+engine = create_engine(f"mysql+pymysql://{USER}:{PASSWD}@{HOST}:{PORT}/{DB}")
